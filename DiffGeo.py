Bug Fix, covariant derivative finished
Found a bug in partial derivatives that the index placement is wrong when doing an infrastructual operation (manipulating without IndexHandle). This bug manifested when testing the corvariant derivative. Hopefully nothing is wrong now.

DiffGeo.py
@@ -4,10 +4,10 @@ import sympy
 
 
 def christoffel(simp=True):
-    i, m, k, l = Index.new("i m k l")
+    i, m, k, l = Index.new("i m k l")  # This function produces pure tensors, so creating indices is safe
     C = Tensor(ind=(CONTRAV, COV, COV))
     C[i, -k, -l] = Tensor.INVERSE_METRIC[i, m] * (
-                D(-k)(Tensor.METRIC[-l, -m]) + D(-m)(Tensor.METRIC[-k, -l]) + D(-l)(Tensor.METRIC[-m, -k])) / 2.
+            D(-k)(Tensor.METRIC[-l, -m]) - D(-m)(Tensor.METRIC[-k, -l]) + D(-l)(Tensor.METRIC[-m, -k])) / 2.
     if simp:
         C.simplify()
     return C
@@ -15,11 +15,30 @@ def christoffel(simp=True):
 
 class Nabla:
     """Implements covariant derivative."""
-    pass
+    connection = christoffel()
+
+    def __init__(self, index):
+        if index.contrv and not Tensor.IMPLICIT_INDEX_RAISING_LOWERING:
+            raise ValueError('The derivative must be covariant.')
+        elif index.contrv:
+            raise NotImplementedError('Not Implemented: implicit index raising/lowering')
+        self.index = index
+
+    def __call__(self, op: IndexHandle):
+        temp_ind = Index.new("temp_index_in_covariant_derivative")[0]  # No index name is gonna crash that one, right??
+        T = Tensor(ind=(*op.tensor.IndexType, COV))
+        T[(*op.ind, self.index)] = D(self.index)(op)
+        for i in range(len(op.ind)):  # messy code... TODO repair this?
+            indl = op.ind[:]
+            indl[i] = temp_ind if op.ind[i].contrv else -temp_ind
+            T[(*op.ind, self.index)] += self.connection[op.ind[i], self.index, -temp_ind] * op.tensor[indl]\
+                if op.ind[i].contrv else -self.connection[temp_ind, self.index, op.ind[i]] * op.tensor[indl]
+        return T[(*op.ind, self.index)]
 
 
 def riemann(simp=True):
     pass
+
 
 if __name__ == "__main__":
     x, y = x[0], x[1]
@@ -27,4 +46,13 @@ if __name__ == "__main__":
     Tensor.METRIC = Tensor(arr=np.array([[sympy.sin(y) ** 2, Real(0)], [Real(0), Real(1)]]), ind=(COV, COV))
     Tensor.INVERSE_METRIC = Tensor(arr=np.array([[1 / sympy.sin(y) ** 2, Real(0)], [Real(0), Real(1)]]),
                                    ind=(CONTRAV, CONTRAV))
-    print(christoffel())
+    a,b,c,d = Index.new('a b c d')
+    print(Tensor.METRIC[-a, -b] * Tensor.INVERSE_METRIC[b, c])  # Metric is normalizing correctly
+    print()
+    Nabla.connection = christoffel()  # This test is based on Carrol(2003). The result is consistent.
+    Nabla.connection.trig_simp()
+    print(Nabla.connection)
+    T = Tensor((COV, COV, COV))
+    T[-a,-b,-c] = Nabla(-a)(Tensor.METRIC[-b, -c])
+    T.trig_simp()
+    print(T)  # Metric compatibility! Hurray!

EinIndex.py
@@ -57,7 +57,7 @@ class IndexHandle:
             icopy = []
             i = list(i)
             inum = 0
-            while True:
+            while True:  # Okay, the code here is extreeeeeemly clumsy. TODO pack this up
                 curind = i[inum]
                 try:
                     cind = i.index(-curind, inum + 1)
@@ -127,9 +127,13 @@ class IndexHandle:
             return IndexHandle(other / self.tensor, self.ind)
         return IndexHandle(other.tensor / self.tensor, (*other.ind, *[-i for i in self.ind]))
 
+    def __neg__(self):
+        return IndexHandle(-self.tensor, self.ind)
+
 
 vec_exp = np.frompyfunc(sympy.expand, 1, 1)
 vec_sym = np.frompyfunc(sympy.simplify, 1, 1)
+vec_trig = np.frompyfunc(sympy.trigsimp, 1, 1)
 
 
 class Tensor:
@@ -219,6 +223,9 @@ class Tensor:
     def expand(self):
         self.T = vec_exp(self.T)
 
+    def trig_simp(self):
+        self.T = vec_trig(self.T)
+
 
 Tensor.METRIC = Tensor(arr=np.array([[Real(1), Real(0)], [Real(0), Real(1)]]), ind=(COV, COV))
 Tensor.INVERSE_METRIC = Tensor(arr=np.array([[Real(1), Real(0)], [Real(0), Real(1)]]), ind=(CONTRAV, CONTRAV))
@@ -239,7 +246,7 @@ class D:
         self.index = index
 
     def __call__(self, op: IndexHandle):
-        return IndexHandle(np.array([self._d(op.tensor.T, i) for i in x]), i=(self.index, *op.ind))
+        return IndexHandle(np.array([self._d(op.tensor.T, i) for i in x]), i=(*op.ind, self.index))
 
 
 if __name__ == '__main__':